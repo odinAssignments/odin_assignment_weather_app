ade9eba0fefe91837182da8d6042b9a2373d844d
SuperCoolNinja <sbpro570@gmail.com>
2025-04-15 20:18:25 +0200

    api is now ready, need to work on UIgit add .

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {
 
 import { getMainWeatherTag } from "./utils/weather_data/index.js";
 
-import { weather_data, forecast_data, weathers_icon } from "./api/index.js";
+import { weather_data, weather_icon } from "./api/index.js";
 
 export async function loadWeatherFromCity(city) {
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline//${city}?key=Secret`;
@@ -20,6 +20,7 @@ export async function loadWeatherFromCity(city) {
     const curr_day = result.days[0].datetime.split("-")[2];
     const curr_month = result.days[0].datetime.split("-")[0];
 
+    // weather_data :
     weather_data.current_degree = `${fahrenheitToCelsius(
       result.days[0].temp
     )}°C`;
@@ -35,21 +36,41 @@ export async function loadWeatherFromCity(city) {
       result.days[0]
     )}, ${monthsNumberToLetter(result.days[0])} ${curr_day}, ${curr_month}`;
 
+    weather_data.uv_index = result.days[0].uvindex;
     weather_data.weather = getMainWeatherTag(result.days[0]);
+    weather_data.humidity = `${result.days[0].humidity.toFixed(0)}%`;
+    weather_data.wind_speed = `${result.days[0].windspeed}Km/h`;
 
+    // forecast_data :
+    const days_forecast_data = [];
+    for (let i = 0; i < 5; i++) {
+      days_forecast_data.push({
+        day: daysNumberToLetter(result.days[i]).slice(0, 3),
+        weather: getMainWeatherTag(result.days[i]),
+        tempMin: `${fahrenheitToCelsius(result.days[i].tempmin)}°C`,
+        tempMax: `${fahrenheitToCelsius(result.days[i].tempmax)}°C`,
+      });
+    }
+
+    console.log("----------- Today's Weather  -----------");
     console.log(weather_data.date);
-
     console.log("current degree :", weather_data.current_degree);
     console.log("degree min : ", weather_data.degree_min);
     console.log("degree max : ", weather_data.degree_max);
     console.log("weather : ", weather_data.weather);
 
+    console.log("----------- Today's Weather Details -----------");
+    console.log("humidity :", weather_data.humidity);
+    console.log("winds speed :", weather_data.wind_speed);
+    console.log("uv index :", weather_data.uv_index);
+
+    console.log("----------- Forecast for 5 days -----------");
+    console.log("days_forecast_data : ", days_forecast_data);
+
     console.log(result);
-    // console.log(result);
-    // console.log(result.currentConditions);
   } catch (error) {
     console.log(error);
   }
 }
 
-loadWeatherFromCity("Namibie");
+loadWeatherFromCity("paris");
